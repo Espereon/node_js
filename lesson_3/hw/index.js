a6f3d46e24e61cecb8fdce0fb8d888f99bf03b3e
Espereon <announcement32@post.com>
2024-11-13 15:16:41 +0300

    hwl3

--- lesson_3/hw/index.js
+++ lesson_3/hw/index.js
@@ -1,18 +1,40 @@
 const express = require('express');
 const fs = require('fs');
-const countMain = 0;
+const path = require('path');
+const counterFilePath = path.join(__dirname, 'Counter.json');
+const Counter = JSON.parse(fs.readFileSync(counterFilePath, 'utf-8'));
+
+// let countMain = 0;
+// let countAbout = 0;
 
 const app = express();
 
+fs.writeFileSync(
+    path.join(__dirname, 'Counter.json'),
+    JSON.stringify(Counter, null, 2)
+);
+
 app.get('/', (req, res) => {
-    res.send('<h1>Добро пожаловать на мой сайт</h1>');
-    // ++count;
-    fs.appendFile('./count.txt', `<p>Количество обращений к основной странице = ${++countMain}`, (err) => {
-        if (err) {
-            console.log(err);
-        }
-        console.log('запись добавлена');
-    });
+    Counter[0].count++;
+    fs.writeFileSync(counterFilePath, JSON.stringify(Counter, null, 2));
+    res.send(
+        `<h1>Главная страница</h1><p>Количество просмотров: ${Counter[0].count}</p><a href = '/about'>Перейти на страницу о сайте</a>`
+    );
+
+    // fs.appendFile('./count.txt', `\nКоличество обращений к основной странице = ${countMain}`, (err) => {
+    //     if (err) {
+    //         console.log(err);
+    //     }
+    //     console.log('запись добавлена');
+    // });
+});
+
+app.get('/about', (req, res) => {
+    Counter[1].count++;
+    fs.writeFileSync(counterFilePath, JSON.stringify(Counter, null, 2));
+    res.send(
+        `<h1>Страница обо мне</h1><p>Количество просмотров: ${Counter[1].count}</p><a href = '/'>Перейти на главную страницу о сайте</a>`
+    );
 });
 
 
